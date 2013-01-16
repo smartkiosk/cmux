e7c2a3c35e78da937428c399efdcd7ccc35e54f5
Sergey Gridassov <grindars@gmail.com>
2013-01-16 21:50:52 +0400

    Marked I/O functions as blocking.

--- lib/cmux/library.rb
+++ lib/cmux/library.rb
@@ -8,12 +8,12 @@ module CMUX
     attach_function :cmux_create,      [ :buffer_out ], :pointer
     attach_function :cmux_destroy,     [ :pointer ], :void
     attach_function :cmux_error,       [ :pointer ], :string
-    attach_function :cmux_open,        [ :pointer, :string ], :int
-    attach_function :cmux_activate,    [ :pointer ], :int
-    attach_function :cmux_open_port,   [ :pointer, :int, :buffer_out ], :int
-    attach_function :cmux_close_port,  [ :pointer, :int ], :int
+    attach_function :cmux_open,        [ :pointer, :string ], :int, :blocking => true
+    attach_function :cmux_activate,    [ :pointer ], :int, :blocking => true
+    attach_function :cmux_open_port,   [ :pointer, :int, :buffer_out ], :int, :blocking => true
+    attach_function :cmux_close_port,  [ :pointer, :int ], :int, :blocking => true
     attach_function :cmux_free,        [ :pointer ], :void
-    attach_function :cmux_open_device, [ :string, :buffer_out ], :int
+    attach_function :cmux_open_device, [ :string, :buffer_out ], :int, :blocking => true
   end
 
 end
